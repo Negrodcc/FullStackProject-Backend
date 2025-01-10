5a3816fd060d6bcac14755c82fb0a0dea292a0f4
Negrodcc <bastian.corrales@ug.uchile.cl>
2025-01-10 18:28:58 -0300

    si

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -62,7 +62,7 @@ const App = () => {
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes cambiadas</h1>
       <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
