51571b5df991bdeab7e07dcc9093beb653b57527
Negrodcc <bastian.corrales@ug.uchile.cl>
2025-01-09 22:31:09 -0300

    add static's frontend files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express()
 
 const cors = require('cors')
 
+app.use(express.static('dist'))
+
 app.use(cors())
 
 let notes = [
@@ -82,7 +84,7 @@ app.get('/api/notes/:id', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
